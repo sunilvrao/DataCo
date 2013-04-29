6a8ac6083d85e2b17daf92071e3a8e6c66d38456
ganesh <ganesh@ganesh-Inspiron-N5010.(none)>
2013-04-29 10:45:53 +0530

    Extracted commenly used js and moved page specific js to yield page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,11 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
-//= require_tree .
+//= require theme-menu
+//= require jquery.easing-1.3.min
+//= require slide-to-top-accordion.min
+//= require bootstrap-progressbar.min
+//= require jquery.isotope.min
+//= require fancybox/source/custom-fancybox-combined
